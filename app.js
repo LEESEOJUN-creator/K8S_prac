3ac4be23fb8bbc00fadac8329509aad29dd03e4d
이서준 <seojun7988@naver.com>
2026-03-28 16:57:13 +0900

    v3으로 업데이트

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const os = require("os");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello Kubernetes v2!");  
+  res.end("Hello Kubernetes v3!");  
 });
 
 server.listen(3000, () => {
